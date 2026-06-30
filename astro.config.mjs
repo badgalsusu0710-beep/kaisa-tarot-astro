cbe62276dfdc2e8d1b27f05291b37ef19cf85fbb
苏尼美 <suyifan@connect.hku.hk>
2026-06-30 10:11:07 +0800

    Set production site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://kaisa-tarot-astro.vercel.app',
   publicDir: './website-assets/public',
   output: 'static',
   trailingSlash: 'always',
